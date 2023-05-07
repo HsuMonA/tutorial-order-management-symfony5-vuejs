93b6f215cb48a848eea93293005a039ad9a17a28
HsuMonA <HsuMonA@users.noreply.github.com>
2023-05-07 12:59:27 +0200

    completion of the form

--- src/Form/CompanyType.php
+++ src/Form/CompanyType.php
@@ -2,29 +2,24 @@
 
 namespace App\Form;
 
-use App\Entity\User;
+use App\Entity\Company;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class UserType extends AbstractType
+class CompanyType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('password', PasswordType::class)
-            ->add('company', CompanyType::class)
-            ->add('submit', SubmitType::class)
+            ->add('name')
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => User::class,
+            'data_class' => Company::class,
         ]);
     }
 }
